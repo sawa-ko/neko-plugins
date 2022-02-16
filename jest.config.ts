f31e9fa051df694a56423bac5ad55e4bed670556
kaname-png <inmortaldragonxspace@outlook.com>
2022-02-16 15:23:08 -0600

    fix: jest config

--- jest.config.ts
+++ jest.config.ts
@@ -6,10 +6,10 @@ export default async (): Promise<Config.InitialOptions> => ({
 	preset: 'ts-jest',
 	testEnvironment: 'node',
 	testRunner: 'jest-circus/runner',
-	testMatch: ['<rootDir>/tests/**/*.test.ts'],
+	testMatch: ['<rootDir>/packages/**/tests/*.test.ts', '<rootDir>/packages/**/tests/*.test.js'],
 	globals: {
 		'ts-jest': {
-			tsconfig: '<rootDir>/tests/tsconfig.json'
+			tsconfig: '<rootDir>/tsconfig.base.json'
 		}
 	}
 });
