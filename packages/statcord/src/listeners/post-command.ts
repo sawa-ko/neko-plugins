8222b2c4a9ba2e8ab41b761580af9565e65ddbea
kaname-png <inmortaldragonxspace@outlook.com>
2022-06-24 23:15:22 -0600

    feat(statcord): updated plugin to @saphire/framework@next

--- packages/statcord/src/listeners/post-command.ts
+++ packages/statcord/src/listeners/post-command.ts
@@ -1,15 +1,29 @@
-import { CommandAcceptedPayload, Events, Listener } from '@sapphire/framework';
+import { MessageCommandAcceptedPayload, ChatInputCommandAcceptedPayload, Events, Listener } from '@sapphire/framework';
 
-export class PluginListener extends Listener<typeof Events.CommandAccepted> {
-	public constructor(context: Listener.Context) {
-		super(context, { event: Events.CommandAccepted, name: 'StatcordPostCommand' });
+export class MessagePluginListener extends Listener<typeof Events.MessageCommandAccepted> {
+	public constructor(context: Listener.Context, options: Listener.Options) {
+		super(context, { ...options, event: Events.MessageCommandAccepted, name: 'StatcordPostMessageCommand' });
 	}
 
-	public run({ command, message }: CommandAcceptedPayload) {
+	public run({ command, message }: MessageCommandAcceptedPayload) {
 		if (this.container.client.options.statcord?.debug) {
-			this.container.logger.debug(`[Statcord-Plugin]: Logging use of the ${command.name} command`);
+			this.container.logger.debug(`[Statcord-Plugin]: Logging use of the ${command.name} message command`);
 		}
 
 		this.container.statcord.postCommand(command.name, message.author.id);
+	}
+}
+
+export class InteractionPluginListener extends Listener<typeof Events.ChatInputCommandAccepted> {
+	public constructor(context: Listener.Context, options: Listener.Options) {
+		super(context, { ...options, event: Events.ChatInputCommandAccepted, name: 'StatcordPostInteractionCommand' });
+	}
+
+	public run({ command, interaction }: ChatInputCommandAcceptedPayload) {
+		if (this.container.client.options.statcord?.debug) {
+			this.container.logger.debug(`[Statcord-Plugin]: Logging use of the ${command.name} interaction command`);
+		}
+
+		this.container.statcord.postCommand(command.name, interaction.user.id);
 	}
 }
