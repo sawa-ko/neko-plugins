a4bed65938c367d1165dd849ec9f96ab72d3e9f3
Sawako <56084970+sawa-ko@users.noreply.github.com>
2023-06-05 22:47:45 +0000

    chore: fix tests

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	displayName: 'unit test',
 	testEnvironment: 'node',
 	testRunner: 'jest-circus/runner',
-	testMatch: ['<rootDir>/tests/**/*.test.ts'],
+	testMatch: ['<rootDir>/packages/**/tests/*.test.ts', '<rootDir>/packages/**/tests/*.test.js'],
 	transform: {
 		"^.+\\.(t|j)sx?$": "@swc/jest",
 	},
--- packages/env/jest.config.js
+++ packages/env/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
 	displayName: 'unit test',
-	preset: 'ts-jest',
 	testEnvironment: 'node',
 	testRunner: 'jest-circus/runner',
 	testMatch: ['<rootDir>/tests/**/*.test.ts'],
