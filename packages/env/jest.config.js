c6d8433102f645a2066dc37068698e3a6aa5665d
Williams Bussat <williams@wbussat.fr>
2023-06-06 10:14:58 +0200

    test: :green_heart: fix jest config for succes ci build

--- packages/env/jest.config.js
+++ packages/env/jest.config.js
@@ -1,9 +1,10 @@
+/** @type {import('jest').Config} */
 module.exports = {
 	displayName: 'unit test',
 	testEnvironment: 'node',
 	testRunner: 'jest-circus/runner',
 	testMatch: ['<rootDir>/tests/**/*.test.ts'],
 	transform: {
-		"^.+\\.(t|j)sx?$": "@swc/jest",
-	},
+		'^.+\\.(t|j)sx?$': '@swc/jest'
+	}
 };
